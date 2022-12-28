265e2ea10dce66f880b9915a5c054f431620c8cd
ArochaKet <dbtech.engineering.3@gmail.com>
2022-12-28 13:20:21 +0700

    แก้ไขให้ Pipe_Main แสดง

--- js/map_controls.js
+++ js/map_controls.js
@@ -139,6 +139,9 @@ $(document).ready(function () {
 
     $('#Pipe_group').prop('checked', false)
     $("#Pipe_group").trigger("change")
+
+    $('#Pipe_Main').prop('checked', true)
+    $("#Pipe_Main").trigger("change")
 /*
     $('#Pipe_Main').prop('checked', true)
     $("#Pipe_Main").trigger("change")
