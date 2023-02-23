089d79404d5ebe495722394f0c243ff414a10313
dbtech5 <dbtech.engineering.5@gmail.com>
2023-02-23 16:18:47 +0700

    สลับตำแหน่งภาพ

--- data.php
+++ data.php
@@ -525,8 +525,8 @@
         <div style="display:flex;">
             <div style="width:30%;padding-left:10%">
                 <div>
-                    <img src="./img/tele/tele_sta.jpg" width="100%" height="200px" style="margin-bottom: 20px;" id="image_ftp">
                     <img src="./img/tele/EAST_20230222_0845.jpg" width="100%" height="200px">
+                    <img src="./img/tele/tele_sta.jpg" width="100%" height="200px" style="margin-bottom: 20px;" id="image_ftp">
                 </div>
             </div>
             <div style="width:70%;margin-left:0;padding-left:2%;">
